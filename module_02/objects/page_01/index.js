b44968b043d93f160316e9dc536d2e18250ff39d
patrick-plue <patrick.pluequett@wbscodingschool.com>
2024-06-07 13:51:44 +0200

    add solution for objectsII

---
+++ module_02/objects/page_01/index.js
@@ -0,0 +1,16 @@
+function Book(title, author, pages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+}
+
+Book.prototype.summary = function () {
+    return `Title: ${this.title}, Author: ${this.author}, Pages: ${
+        this.pages
+    }, Read: ${this.isRead ? 'Yes' : 'No'}`;
+};
+
+const myBook = new Book('To Kill a Mockingbird', 'Harper Lee', 281, true);
+
+console.log(myBook.summary());
